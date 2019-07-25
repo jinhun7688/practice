fbdaa20d77cd189fe5c17f2e975283067005d0e0
eunhwa Choi <eunhxa@gmail.com>
2019-07-25 16:25:43 +0900

    메세지르 꼭 적어야하니?

--- test.py
+++ test.py
@@ -1,23 +1,11 @@
-dddddddddddfuxt
-
 HeeSang is Sad :(
 
 Cheer up HeeSang
-
-sdkqowdkpqwkdopqwkdpoqkwopd
 
 print('My Git Project')
 
 print('HeeSang is Sad :(')
 
-print('하이루 방가방가')
-	print('시바려)'
-
-            print('강사늼?")
-	
-	print('hello heesang!')
 print('hello heesang!')
 
-fuck you~^^
 
-빡대가리친구야
